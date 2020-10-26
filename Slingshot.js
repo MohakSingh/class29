31e2a2a66e9d2d97d131e5312433a8c3e49c6965
MohakSingh <mohaksingh311@gmail.com>
2020-10-26 16:40:20 +0530

    added images to the sling shot

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,10 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        this.sling1=loadImage("sprites/sling1.png");
+        this.sling2=loadImage("sprites/sling2.png");
+        this.sling3=loadImage("sprites/sling3.png");
+
     }
 
     fly(){
@@ -16,11 +20,22 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1,200,40,40,180);
+        image(this.sling2,175,45,40,100);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(42,22,8);
+            if(pointA.x<220){
+            line(pointA.x-25, pointA.y, pointB.x-10, pointB.y+25);
+            line(pointA.x-25, pointA.y, pointB.x+25, pointB.y+20);
+            image(this.sling3,pointA.x-30,pointA.y,15,20)}
+            else{
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y+25);
+                line(pointA.x+25, pointA.y, pointB.x+25, pointB.y+20);
+                image(this.sling3,pointA.x+25,pointA.y,15,20)
+            }
         }
     }
     
